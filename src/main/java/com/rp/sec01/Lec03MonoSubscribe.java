6f3131d38e4892c3338ce4b4609ca0386ae30521
kshneo0 <kshneo@gmail.com>
2022-04-01 17:44:28 +0900

    17. Simple Refactor

--- src/main/java/com/rp/sec01/Lec03MonoSubscribe.java
+++ src/main/java/com/rp/sec01/Lec03MonoSubscribe.java
@@ -2,6 +2,8 @@ package com.rp.sec01;
 
 import java.awt.event.ItemEvent;
 
+import com.rp.courseutil.Util;
+
 import reactor.core.publisher.Mono;
 
 public class Lec03MonoSubscribe {
@@ -13,6 +15,7 @@ public class Lec03MonoSubscribe {
 		
 		Mono<Integer>mono = Mono.just("ball")
 				   .map(String::length)
+//				   .map(l -> l/1);
 				   .map(l -> l/0);
 		
 		// 1
@@ -20,9 +23,9 @@ public class Lec03MonoSubscribe {
 		
 		//2
 		mono.subscribe(
-			item -> System.out.println(item),
-			err -> System.out.println(err.getMessage()),
-			()-> System.out.println("Completed")			
+			Util.onNext(),
+			Util.onError(),
+			Util.onComplete()		
 		);
 	}
 
