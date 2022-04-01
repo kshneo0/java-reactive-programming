58fb556a8e30eae809d9bb276339a7725386ea14
kshneo0 <kshneo@gmail.com>
2022-04-02 06:04:47 +0900

    20. [Mono] - From Supplier

---
+++ src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
@@ -0,0 +1,28 @@
+package com.rp.sec01;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Mono;
+
+public class Lec05MonoFromSupplier {
+
+	public static void main(String[] args) {
+		
+		
+		// use just only when you have data already
+		// Mono<String> mono = Mono.just(getName());
+		
+		Mono<String> mono = Mono.fromSupplier(() -> getName());
+		mono.subscribe(
+				Util.onNext()
+		);
+		
+
+	}
+	
+	private static String getName() {
+		System.out.println("Generating name..");
+		return Util.faker().name().fullName();
+	}
+
+}
