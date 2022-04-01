477c669f68fd48e4ee4c5d2769a4a4f27ced022a
kshneo0 <kshneo@gmail.com>
2022-04-02 07:49:03 +0900

    22. [Mono] - Pipeline Build vs Execution

--- src/main/java/com/rp/courseutil/Util.java
+++ src/main/java/com/rp/courseutil/Util.java
@@ -21,4 +21,12 @@ public class Util {
 	public static Faker faker() {
 		return FAKER;
 	}
+	public static void sleepSeconds(int seconds) {
+		try {
+			Thread.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
