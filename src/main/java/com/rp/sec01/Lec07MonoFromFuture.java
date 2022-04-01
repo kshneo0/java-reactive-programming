dcd80d6de2d2492f3b5167ad35c289f6c72c8822
kshneo0 <kshneo@gmail.com>
2022-04-02 08:19:53 +0900

    25. [Mono] - From Future

---
+++ src/main/java/com/rp/sec01/Lec07MonoFromFuture.java
@@ -0,0 +1,24 @@
+package com.rp.sec01;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Mono;
+
+public class Lec07MonoFromFuture {
+
+    public static void main(String[] args) {
+
+        Mono.fromFuture(getName())
+                .subscribe(Util.onNext());
+
+        Util.sleepSeconds(1);
+
+    }
+
+    private static CompletableFuture<String> getName(){
+        return CompletableFuture.supplyAsync(() -> Util.faker().name().fullName());
+    }
+
+}
