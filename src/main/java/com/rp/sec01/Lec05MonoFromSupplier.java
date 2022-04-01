c73dff0116026e4d3740817c821dcaf41ab94c3f
kshneo0 <kshneo@gmail.com>
2022-04-02 07:37:32 +0900

    21. [Mono] - From Callable

--- src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
+++ src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
@@ -1,5 +1,8 @@
 package com.rp.sec01;
 
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
 import com.rp.courseutil.Util;
 
 import reactor.core.publisher.Mono;
@@ -11,11 +14,28 @@ public class Lec05MonoFromSupplier {
 		
 		// use just only when you have data already
 		// Mono<String> mono = Mono.just(getName());
-		
+/*		
 		Mono<String> mono = Mono.fromSupplier(() -> getName());
+		mono.subscribe(
+			Util.onNext()
+		);
+*/		
+		Supplier<String> stringSupplier = () -> getName();
+		
+		Mono<String> mono = Mono.fromSupplier(stringSupplier);
 		mono.subscribe(
 				Util.onNext()
 		);
+		
+		System.out.println("----------");
+		
+		Callable<String> stirngCallable = () -> getName();
+		Mono.fromCallable(stirngCallable)
+			.subscribe(
+					Util.onNext()
+			);
+			
+		
 		
 
 	}
