2eb66d8a8e24e71700029a0d284430aac56c2f87
kshneo0 <kshneo@gmail.com>
2022-04-02 19:58:29 +0900

    48. Flux Create - Demo

---
+++ src/main/java/com/rp/sec03/Lec01FluxCreate.java
@@ -0,0 +1,35 @@
+package com.rp.sec03;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Lec01FluxCreate {
+
+	public static void main(String[] args) {
+		
+		Flux.create(fluxSink -> {
+/*			
+			fluxSink.next(1);
+			fluxSink.next(2);
+			fluxSink.complete();
+*/
+/*
+			for( int i =0; i< 10; i++) {
+				fluxSink.next(Util.faker().country().name());
+			}
+			fluxSink.complete();
+*/
+			String country;
+			
+			do {
+				country = Util.faker().country().name();
+				fluxSink.next(country);
+			} while(!country.toLowerCase().equals("canada"));
+			fluxSink.complete();
+			
+		}).subscribe(Util.subscriber());
+
+	}
+
+}
