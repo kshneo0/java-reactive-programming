1dc3a6aa53d3a64c6679f3cd89841400d7ca424f
kshneo0 <kshneo@gmail.com>
2022-04-02 16:02:16 +0900

    32. [Flux] - Just

---
+++ src/main/java/com/rp/sec02/Lec01FluxIntro.java
@@ -0,0 +1,33 @@
+package com.rp.sec02;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Lec01FluxIntro {
+
+	public static void main(String[] args) {
+		
+//		Flux<Object> flux = Flux.just(1);
+		
+//		Flux<Object> flux = Flux.just(1,2,3,4);
+		
+//      flux.subscribe( Util.onNext() );
+		
+//		Flux<Object> flux = Flux.empty();
+//		
+//        flux.subscribe(
+//                Util.onNext(),
+//                Util.onError(),
+//                Util.onComplete());
+        
+		Flux<Object> flux = Flux.just(1,2, 3, "a", Util.faker().name().fullName());
+
+        flux.subscribe(
+                Util.onNext(),
+                Util.onError(),
+                Util.onComplete());
+                
+	}
+
+}
