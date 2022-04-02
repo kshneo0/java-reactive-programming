f14f4418078ad1860249e1319d75a1d1d3938cb2
kshneo0 <kshneo@gmail.com>
2022-04-02 19:05:46 +0900

    42. [Flux] - To Mono

--- src/main/java/com/rp/sec02/Lec09FluxFromMono.java
+++ src/main/java/com/rp/sec02/Lec09FluxFromMono.java
@@ -8,12 +8,21 @@ import reactor.core.publisher.Mono;
 public class Lec09FluxFromMono {
 
 	public static void main(String[] args) {
-		
+
+		/*
 		Mono<String> mono = Mono.just("a");
 		
 		Flux<String> flux = Flux.from(mono);
 		
 		flux.subscribe(Util.onNext());
+		*/
+		
+		Flux.range(1,10)
+			.filter(i-> i > 3)
+			.next()
+			.subscribe(Util.onNext(),
+					Util.onError(),
+					Util.onComplete());
 
 	}
 	
