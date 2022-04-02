c3055dd5af37341830919b168941de3f87a362bb
kshneo0 <kshneo@gmail.com>
2022-04-02 18:21:37 +0900

    36. [Flux] - Range

---
+++ src/main/java/com/rp/sec02/Lec05FluxRange.java
@@ -0,0 +1,19 @@
+package com.rp.sec02;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Lec05FluxRange {
+
+	public static void main(String[] args) {
+		
+		Flux.range(3,10)
+			.map(i -> Util.faker().name().fullName())
+			.subscribe(
+				Util.onNext()
+			);
+		
+	}
+
+}
