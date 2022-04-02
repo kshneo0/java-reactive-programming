ade794682f9e2844b1140aea4379861d716a5d79
kshneo0 <kshneo@gmail.com>
2022-04-02 19:28:26 +0900

    45. [ASSIGNMENT-SOLUTION] - Stock Price Observer Implementation

--- src/main/java/com/rp/sec02/Lec09FluxFromMono.java
+++ src/main/java/com/rp/sec02/Lec09FluxFromMono.java
@@ -19,7 +19,7 @@ public class Lec09FluxFromMono {
 		
 		Flux.range(1,10)
 			.filter(i-> i > 3)
-			.next()
+			.next()	// 1
 			.subscribe(Util.onNext(),
 					Util.onError(),
 					Util.onComplete());
