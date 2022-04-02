86576049e0a42a67e5c63a73bd1282cb90f83f08
kshneo0 <kshneo@gmail.com>
2022-04-02 18:26:31 +0900

    37. [Flux] - Log

--- src/main/java/com/rp/sec02/Lec05FluxRange.java
+++ src/main/java/com/rp/sec02/Lec05FluxRange.java
@@ -9,7 +9,9 @@ public class Lec05FluxRange {
 	public static void main(String[] args) {
 		
 		Flux.range(3,10)
+			.log()
 			.map(i -> Util.faker().name().fullName())
+			.log()
 			.subscribe(
 				Util.onNext()
 			);
