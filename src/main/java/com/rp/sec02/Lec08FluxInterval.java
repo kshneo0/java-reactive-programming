272d8cc31c8c69dfe1bd8237f309e82df1a61f08
kshneo0 <kshneo@gmail.com>
2022-04-02 18:56:57 +0900

    40. [Flux] - Interval

---
+++ src/main/java/com/rp/sec02/Lec08FluxInterval.java
@@ -0,0 +1,21 @@
+package com.rp.sec02;
+
+import java.time.Duration;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Lec08FluxInterval {
+
+	public static void main(String[] args) {
+		
+		Flux.interval(Duration.ofSeconds(1))
+			.log()
+			.subscribe(Util.onNext());
+		
+		Util.sleepSeconds(5);
+
+	}
+
+}
