1d464f7697398dbb3fa768b7b8eca51aa9a69d72
kshneo0 <kshneo@gmail.com>
2022-04-03 09:26:39 +0900

    66. Operator - Do Hooks / Callbacks - Part 2

--- src/main/java/com/rp/sec04/Lec03DoCallbacks.java
+++ src/main/java/com/rp/sec04/Lec03DoCallbacks.java
@@ -13,19 +13,24 @@ public class Lec03DoCallbacks {
 			for (int i = 0; i < 5; i++ ) {
 				fluxSink.next(i);
 			}
-			fluxSink.complete();
+			// fluxSink.complete();
+			fluxSink.error(new RuntimeException("oops"));
 			System.out.println("--completed");
 		})
 		.doOnComplete(() -> System.out.println("doOnComplete"))
-		.doFirst(() -> System.out.println("doFirst"))
+		.doFirst(() -> System.out.println("doFirst 1"))
 		.doOnNext(o -> System.out.println("doOnNext : " + o))
-		.doOnSubscribe(s -> System.out.println("doOnSubscribe " + s))
+		.doOnSubscribe(s -> System.out.println("doOnSubscribe 1 " + s))
 		.doOnRequest(l -> System.out.println("doOnRequest : " + l))
+		.doFirst(() -> System.out.println("doFirst 2"))
 		.doOnError(err -> System.out.println("doOnError : " + err.getMessage()))
 		.doOnTerminate(() -> System.out.println("doOnTerminate"))
 		.doOnCancel(() -> System.out.println("doOnCancel"))
+		.doOnSubscribe(s -> System.out.println("doOnSubscribe 2 " + s))
 		.doFinally(signal -> System.out.println("doFinally : " + signal))
+		.doFirst(() -> System.out.println("doFirst 3"))
 		.doOnDiscard(Object.class, o -> System.out.println("doOnDiscard : " + o))
+		.take(2)
 		.subscribe(Util.subscriber());
 
 	}
