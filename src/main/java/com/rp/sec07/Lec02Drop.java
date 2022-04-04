29517200f55c1c405a9877af81cce991ff63ca73
kshneo0 <kshneo@gmail.com>
2022-04-04 09:23:02 +0900

    105. Overflow Strategy - Drop - Part 2

--- src/main/java/com/rp/sec07/Lec02Drop.java
+++ src/main/java/com/rp/sec07/Lec02Drop.java
@@ -9,10 +9,14 @@ public class Lec02Drop {
 
 	public static void main(String[] args) {
 		
+		// 75% 12
+        System.setProperty("reactor.bufferSize.small", "16");
+		
 		Flux.create(fluxSink -> {
 			for (int i = 0; i < 501; i++ ) {
 				fluxSink.next(i);
 				System.out.println("Pushed : " + i);
+				Util.sleepMillis(1);
 			}
 			fluxSink.complete();
 		})
