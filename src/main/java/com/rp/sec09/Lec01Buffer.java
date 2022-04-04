0bb63702ac5bf82541b9b949a2b8c0dee2df2a19
kshneo0 <kshneo@gmail.com>
2022-04-04 15:20:22 +0900

    123. Batching With Buffer

---
+++ src/main/java/com/rp/sec09/Lec01Buffer.java
@@ -0,0 +1,26 @@
+package com.rp.sec09;
+
+import java.time.Duration;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Lec01Buffer {
+
+	public static void main(String[] args) {
+		
+		eventStream()
+			.buffer(5)
+			.subscribe(Util.subscriber());
+		
+		Util.sleepSeconds(60);
+
+	}
+	
+	private static Flux<String> eventStream(){
+		return Flux.interval(Duration.ofMillis(300))
+					.map( i -> "event"+i);
+	}
+
+}
