923f018f277440786e78f2bee4fe6066f0905ecc
kshneo0 <kshneo@gmail.com>
2022-04-04 15:28:18 +0900

    125. Batching With Buffer - Timeout

--- src/main/java/com/rp/sec09/Lec01Buffer.java
+++ src/main/java/com/rp/sec09/Lec01Buffer.java
@@ -11,7 +11,9 @@ public class Lec01Buffer {
 	public static void main(String[] args) {
 		
 		eventStream()
-			.buffer(5)
+//			.buffer(5)
+//			.buffer(Duration.ofSeconds(2))
+			.bufferTimeout(5, Duration.ofSeconds(2))
 			.subscribe(Util.subscriber());
 		
 		Util.sleepSeconds(60);
@@ -19,8 +21,7 @@ public class Lec01Buffer {
 	}
 	
 	private static Flux<String> eventStream(){
-		return Flux.interval(Duration.ofMillis(300))
-					.take(3)
+		return Flux.interval(Duration.ofMillis(800))
 					.map( i -> "event"+i);
 	}
 
