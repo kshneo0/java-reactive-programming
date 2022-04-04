52cb25e6645d6562cfdbb5b59a2f05df1f47d52f
kshneo0 <kshneo@gmail.com>
2022-04-04 19:02:11 +0900

    153. Sink Many - Multicast - Buffer

--- src/main/java/com/rp/sec11/Lec04SinkMulti.java
+++ src/main/java/com/rp/sec11/Lec04SinkMulti.java
@@ -15,12 +15,14 @@ public class Lec04SinkMulti {
         // handle through which subscribers will receive items
         Flux<Object> flux = sink.asFlux();
  
-        flux.subscribe(Util.subscriber("sam"));
-        flux.subscribe(Util.subscriber("mike"));
-
         sink.tryEmitNext("hi");
         sink.tryEmitNext("how are you");
-        sink.tryEmitNext("?");
 
+        flux.subscribe(Util.subscriber("sam"));
+        flux.subscribe(Util.subscriber("mike"));
+        sink.tryEmitNext("?");
+        flux.subscribe(Util.subscriber("jake"));
+
+        sink.tryEmitNext("new msg");
     }
 }
