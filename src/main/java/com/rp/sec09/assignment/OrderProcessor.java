580854ef4a74fb6baa41a1a0e63210638178a2e5
kshneo0 <kshneo@gmail.com>
2022-04-04 16:42:03 +0900

    136. Additional Note On Assignment

--- src/main/java/com/rp/sec09/assignment/OrderProcessor.java
+++ src/main/java/com/rp/sec09/assignment/OrderProcessor.java
@@ -16,19 +16,17 @@ public class OrderProcessor {
     public static Function<Flux<PurchaseOrder>, Flux<PurchaseOrder>> kidsProcessing() {
         return flux -> flux
                 .doOnNext(p -> p.setPrice(0.5 * p.getPrice()))
-                .flatMap(p -> Flux.just(p, getFreeKidsOrder()));
-//                .flatMap(p -> Flux.concat(Mono.just(p), getFreeKidsOrder()));
+                .flatMap(p -> Flux.concat(Mono.just(p), getFreeKidsOrder()));
     }
 
-//    private static Mono<PurchaseOrder> getFreeKidsOrder(){
-   	private static PurchaseOrder getFreeKidsOrder(){
-//        return Mono.fromSupplier(() -> {
+    private static Mono<PurchaseOrder> getFreeKidsOrder(){
+        return Mono.fromSupplier(() -> {
             PurchaseOrder purchaseOrder = new PurchaseOrder();
             purchaseOrder.setItem("FREE - " + purchaseOrder.getItem());
             purchaseOrder.setPrice(0);
             purchaseOrder.setCategory("Kids");
             return purchaseOrder;
-//        });
+        });
     }
 
 
