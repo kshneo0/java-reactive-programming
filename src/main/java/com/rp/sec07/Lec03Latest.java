a9b9747d4d99252d9c08dbd15380988639400947
kshneo0 <kshneo@gmail.com>
2022-04-04 09:33:28 +0900

    107. Overflow Strategy - Latest

---
+++ src/main/java/com/rp/sec07/Lec03Latest.java
@@ -0,0 +1,36 @@
+package com.rp.sec07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec03Latest {
+
+	public static void main(String[] args) {
+		// 75% 12
+        System.setProperty("reactor.bufferSize.small", "16");
+		
+		Flux.create(fluxSink -> {
+			for (int i = 0; i < 201; i++ ) {
+				fluxSink.next(i);
+				System.out.println("Pushed : " + i);
+				Util.sleepMillis(1);
+			}
+			fluxSink.complete();
+		})
+			.onBackpressureLatest()
+			.publishOn(Schedulers.boundedElastic())
+			.doOnNext( i-> {
+				Util.sleepMillis(10);
+			})
+			.subscribe(Util.subscriber());
+		
+		Util.sleepSeconds(10);
+
+	}
+
+}
