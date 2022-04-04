23de7693a4f71280c1cd3f044c6ea47a549176f2
kshneo0 <kshneo@gmail.com>
2022-04-04 20:29:37 +0900

    174. Step Verifier - Virtual Time Test - Part 2

--- src/test/java/com/rp/test/Lec05VirtualTimeTest.java
+++ src/test/java/com/rp/test/Lec05VirtualTimeTest.java
@@ -20,6 +20,16 @@ public class Lec05VirtualTimeTest {
                 .expectNext("1a", "2a", "3a", "4a")
                 .verifyComplete();
     }
+    
+    @Test
+    public void test2(){
+        StepVerifier.withVirtualTime(() -> timeConsumingFlux())
+                .expectSubscription() // sub is an event
+                .expectNoEvent(Duration.ofSeconds(4))
+                .thenAwait(Duration.ofSeconds(20))
+                .expectNext("1a", "2a", "3a", "4a")
+                .verifyComplete();
+    }
 
     private Flux<String> timeConsumingFlux(){
         return Flux.range(1, 4)
