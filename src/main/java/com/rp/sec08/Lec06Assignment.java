0328896dc941c010633201377e9c790e869652e3
kshneo0 <kshneo@gmail.com>
2022-04-04 15:02:13 +0900

    121. [ASSIGNMENT-SOLUTION] - Car Price Over Time Stream - Implementation

---
+++ src/main/java/com/rp/sec08/Lec06Assignment.java
@@ -0,0 +1,33 @@
+package com.rp.sec08;
+
+import java.time.Duration;
+
+import com.rp.courseutil.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Lec06Assignment {
+
+	public static void main(String[] args) {
+		
+		final int carPrice = 10_000;
+		Flux.combineLatest(monthStream(), demandStream(), (month, demand) -> {
+			return (carPrice - (month * 100)) * demand;
+		})
+		.subscribe(Util.subscriber());
+		
+		Util.sleepSeconds(20);
+
+	}
+	
+	public static Flux<Long> monthStream(){
+		return Flux.interval(Duration.ZERO, Duration.ofSeconds(1));	
+	}
+	
+	private static Flux<Double> demandStream(){
+		return Flux.interval(Duration.ofSeconds(3))
+				.map(i -> Util.faker().random().nextInt(80,120)/100d)
+				.startWith(1d);
+	}
+
+}
