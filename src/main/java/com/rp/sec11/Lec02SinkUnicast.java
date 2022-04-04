42c357954eca1ff77defb6100acda6d9e46831f1
kshneo0 <kshneo@gmail.com>
2022-04-04 18:47:08 +0900

    149. Sink Many - Unicast

---
+++ src/main/java/com/rp/sec11/Lec02SinkUnicast.java
@@ -0,0 +1,27 @@
+package com.rp.sec11;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import reactor.core.publisher.Sinks.Many;
+
+public class Lec02SinkUnicast {
+
+    public static void main(String[] args) {
+
+        // handle through which we would push items
+        Many<Object> sink = Sinks.many().unicast().onBackpressureBuffer();
+
+        // handle through which subscribers will receive items
+        Flux<Object> flux = sink.asFlux();
+
+        flux.subscribe(Util.subscriber("sam"));
+
+        sink.tryEmitNext("hi");
+        sink.tryEmitNext("how are you");
+        sink.tryEmitNext("?");
+
+
+    }
+
+}
